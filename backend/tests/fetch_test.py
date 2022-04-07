fed9edf25a820484b7cf24e67981007ac708c522
degregory <virologistd@gmail.com>
2022-04-07 11:02:59 -0500

    updated testing format

--- backend/tests/fetch_test.py
+++ backend/tests/fetch_test.py
@@ -12,34 +12,48 @@ import pytest
 sys.path.insert(0, os.getcwd().split('SHED')[0]+'SHED/backend' )
 import modules.sra_fetch as fetch
 
-def test_get_accessions():
+class TestGetAccessions:
 # testing get_accessions function
-# bad filename
-    assert fetch.get_accessions('probably.a.non-existing.file') == 1
 
-# empty file
-    assert fetch.get_accessions('./tests/TestEmptySraList.txt') == 2
+    def test_non_existing_file_get(self):
+    # bad filename
+        assert fetch.get_accessions('probably.a.non-existing.file') == 1
 
-# no valid accessions
-    assert fetch.get_accessions('./tests/TestBadSraList.txt') == 2
+    def test_empty_file_get(self):
+    # empty file
+        assert fetch.get_accessions('./tests/TestEmptySraList.txt') == 2
 
-# no real accessions
-    assert fetch.get_accessions('./tests/TestBadSraList2.txt') == ['SRR00000001']
+    def test_bad_sra_acc_get(self):
+    # no valid accessions
+        assert fetch.get_accessions('./tests/TestBadSraList.txt') == 2
 
-# good file
-    assert fetch.get_accessions('./tests/TestMixedSraList.txt') == ['ERR5019844', 'SRR15294802', 'SRR17888010', 'SRR15240439', 'SRR17887900']
+    def test_fake_sra_acc_get(self):
+    # no real accessions
+        assert fetch.get_accessions('./tests/TestBadSraList2.txt') == ['SRR00000001']
 
-def test_fetching():
+    def test_good_sra_acc_get(self):
+    # good file
+        assert fetch.get_accessions('./tests/TestMixedSraList.txt') == ['ERR5019844', 'SRR15294802', 'SRR17888010', 'SRR15240439', 'SRR17887900']
+
+class TestFetching:
 # testing fetching function
-# empty list
-    assert fetch.fetching([]) == 1
-# not a list
-    assert fetch.fetching(2) == 1
-# list of non strings, error handled by sra toolkit
-    assert fetch.fetching([2, ['a']]) == 0
-# fake SRA accession, error handled by sra toolkit
-    assert fetch.fetching(['SRR00000001']) == 0
-# good SRA accession
-    assert fetch.fetching(['SRR17887900']) == 0
 
+    def test_empty_list_fetch(self):
+    # empty list
+        assert fetch.fetching([]) == 1
 
+    def test_not_a_list_fetch(self):
+    # not a list
+        assert fetch.fetching(2) == 1
+
+    def test_non_string_list_fetch(self):
+    # list of non strings, error handled by sra toolkit
+        assert fetch.fetching([2, ['a']]) == 0
+
+    def test_fake_sra_acc_fetch(self):
+    # fake SRA accession, error handled by sra toolkit
+        assert fetch.fetching(['SRR00000001']) == 0
+
+    def test_good_sra_acc_fetch(self):
+    # good SRA accession
+        assert fetch.fetching(['SRR17887900']) == 0
