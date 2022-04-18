2315b71c3ab06327e0ccd6a8ddc802e70bf13b17
degregory <virologistd@gmail.com>
2022-04-18 15:17:31 -0700

    added update options

--- backend/configure.sh
+++ backend/configure.sh
@@ -3,7 +3,7 @@
 # written by Devon Gregory
 # install dependencies
 # uses conda to install most of the programs
-# last edited on 4-9-22
+# last edited on 4-18-22
 
 # commented out req installs for yet to be implimented parts of pipeline
 
@@ -40,20 +40,27 @@ fi
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
+conda config --add channels agbiome
 
-# # install programs
+# # install programs 
+# # todo: add install checks for packages to choose install or update
 # # cut_adapt https://github.com/marcelm/cutadapt
 # fastx tools http://hannonlab.cshl.edu/fastx_toolkit/license.html
 # # minimap2 https://github.com/lh3/minimap2
 # bbtools https://github.com/kbaseapps/BBTools
 # # sam refiner https://github.com/degregory/SAM_Refiner
 # pytest https://docs.pytest.org/en/7.1.x/contents.html
-# conda install -c bioconda cutadapt
+conda install -c bioconda cutadapt
 conda install -c bioconda fastx_toolkit
 # conda install -c bioconda minimap2
 conda install -c agbiome bbtools
 conda install -c conda-forge pytest
 conda install -c bioconda sra-tools
+conda update -c bioconda fastx_toolkit
+# conda update -c bioconda minimap2
+conda update -c agbiome bbtools
+conda update -c conda-forge pytest
+conda update -c bioconda sra-tools
 # if [ ! -f ./SAM_Refiner.py]
 	# then
 	# wget https://raw.githubusercontent.com/degregory/SAM_Refiner/main/SAM_Refiner.py
