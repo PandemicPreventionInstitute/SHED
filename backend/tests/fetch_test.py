76c9a055714c524dadc9b90f96fec3ee82289171
degregory <virologistd@gmail.com>
2022-04-21 10:56:59 -0700

    fix paths in tests and conda call for repair.sh

--- backend/tests/fetch_test.py
+++ backend/tests/fetch_test.py
@@ -1,41 +1,44 @@
 #!/bin/env python3
 
-'''
+"""
 Writen by Devon Gregory
 This script tests the workings of the sra_fetch module using pytest
-Last edited on 4-14-22
-'''
+Last edited on 4-21-22
+"""
 
 import os
 import sys
 import pytest
-sys.path.insert(0, os.getcwd().split('SHED')[0]+'SHED/backend' )
+
+sys.path.insert(0, os.getcwd().split("SHED")[0] + "SHED/backend")
 import modules.sra_fetch as fetch
 
-base_path = os.getcwd().split('SHED')[0]+'SHED/backend/tests/'
+base_path = os.getcwd().split("SHED")[0] + "SHED/backend/tests/"
+
 
 class TestFetching:
-    '''Tests of get_fastqs function in sra_fetch.  All will fail if NCBI SRA can't be reached.'''
+    """Tests of get_fastqs function in sra_fetch.  All will fail if NCBI SRA can't be reached."""
+
     def test_empty_filename_fetch(self):
-    # empty filename
-        assert fetch.get_fastqs(base_path, '') == -1
+        # empty filename
+        assert fetch.get_fastqs(base_path, "") == -1
 
     def test_not_a_str_fetch(self):
-    # not a string
+        # not a string
         assert fetch.get_fastqs(base_path, 2) == -1
 
     def test_fake_sra_acc_fetch(self):
-    # fake SRA accession, error handled by sra toolkit
-        assert fetch.get_fastqs(base_path, 'SRR00000001') == (768, 768)
+        # fake SRA accession, error handled by sra toolkit
+        assert fetch.get_fastqs(base_path, "SRR00000001") == (768, 768)
         os.remove(f"{base_path}fastqs/SRR00000001.fetch.started")
 
     def test_good_sra_acc_fetch(self):
-    # good SRA accession
-        assert fetch.get_fastqs(base_path, 'SRR17887900') == (0, 0)
+        # good SRA accession
+        assert fetch.get_fastqs(base_path, "SRR17887900") == (0, 0)
         os.remove(f"{base_path}fastqs/SRR17887900_1.fastq.gz")
         os.remove(f"{base_path}fastqs/SRR17887900_2.fastq.gz")
         os.system(f"rm -rf {base_path}SRAs/")
 
     def test_already_fetched_sra_acc_fetch(self):
-    # good SRA accession
-        assert fetch.get_fastqs(base_path, 'paired.test') == (0, 0)
+        # good SRA accession
+        assert fetch.get_fastqs(base_path, "paired.test") == (0, 0)
--- backend/tests/file_parse_test.py
+++ backend/tests/file_parse_test.py
@@ -1,61 +1,80 @@
 #!/bin/env python3
 
-'''
+"""
 Writen by Devon Gregory
 This script tests the workings of the sra_fetch module using pytest
-Last edited on 4-14-22
-'''
+Last edited on 4-21-22
+"""
 
 import os
 import sys
 import pytest
-sys.path.insert(0, os.getcwd().split('SHED')[0]+'SHED/backend' )
+
+sys.path.insert(0, os.getcwd().split("SHED")[0] + "SHED/backend")
 import modules.sra_file_parse as file_parse
 
-base_path = os.getcwd().split('SHED')[0]+'SHED/backend/tests/'
+base_path = os.getcwd().split("SHED")[0] + "SHED/backend/tests/"
+
 
 class TestGetAccessions:
-    '''Tests of get_accessions function in sra_file_parse'''
+    """Tests of get_accessions function in sra_file_parse"""
 
     def test_non_existing_file_get(self):
-    # bad filename
-        assert file_parse.get_accessions('probably.a.non-existing.file') == 1
+        # bad filename
+        assert file_parse.get_accessions("probably.a.non-existing.file") == 1
 
     def test_empty_file_get(self):
-    # empty file
-        assert file_parse.get_accessions('./tests/TestEmptySraList.txt') == 2
+        # empty file
+        assert file_parse.get_accessions(f"{base_path}TestEmptySraList.txt") == 2
 
     def test_bad_sra_acc_get(self):
-    # no valid accessions
-        assert file_parse.get_accessions('./tests/TestBadSraList.txt') == 2
+        # no valid accessions
+        assert file_parse.get_accessions(f"{base_path}TestBadSraList.txt") == 2
 
     def test_fake_sra_acc_get(self):
-    # no real accessions
-        assert file_parse.get_accessions('./tests/TestBadSraList2.txt') == ['SRR00000001']
+        # no real accessions
+        assert file_parse.get_accessions(f"{base_path}TestBadSraList2.txt") == [
+            "SRR00000001"
+        ]
 
     def test_good_sra_acc_get(self):
-    # good file, mix of good and bad SRAs
-        assert file_parse.get_accessions('./tests/TestMixedSraList.txt') == ['ERR5019844', 'SRR15294802', 'SRR17888010', 'SRR15240439', 'SRR17887900']
+        # good file, mix of good and bad SRAs
+        assert file_parse.get_accessions(f"{base_path}TestMixedSraList.txt") == [
+            "ERR5019844",
+            "SRR15294802",
+            "SRR17888010",
+            "SRR15240439",
+            "SRR17887900",
+        ]
 
 
 class TestFindFastqs:
-    ''' testing find_fastqs function in sra_file_parse'''
-
+    """testing find_fastqs function in sra_file_parse"""
 
     def test_no_fastqs(self):
-    # no fastqs
-        assert file_parse.find_fastqs(base_path, 'not.an.sra') == ()
+        # no fastqs
+        assert file_parse.find_fastqs(base_path, "not.an.sra") == ()
 
     def test_single_fastqs(self):
-    # single fastqs
-        single_tuple = tuple(['/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/single.test_1.fastq.gz'])
-        assert file_parse.find_fastqs(base_path, 'single.test') == single_tuple
+        # single fastqs
+        single_tuple = tuple([f"{base_path}fastqs/single.test_1.fastq.gz"])
+        assert file_parse.find_fastqs(base_path, "single.test") == single_tuple
 
     def test_paired_fastqs(self):
-    # paired fastqs
-        assert file_parse.find_fastqs(base_path, 'paired.test') \
-            == (('/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/paired.test_1.fastq.gz', '/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/paired.test_2.fastq.gz'))
+        # paired fastqs
+        assert file_parse.find_fastqs(base_path, "paired.test") == (
+            (
+                f"{base_path}fastqs/paired.test_1.fastq.gz",
+                f"{base_path}fastqs/paired.test_2.fastq.gz",
+            )
+        )
 
     def test_mismatched_fastqs(self):
-    # single and paired
-        assert file_parse.find_fastqs(base_path, '3fastqtest') == (('/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/3fastqtest_1.fastq.gz', '/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/3fastqtest_2.fastq.gz', '/mnt/e/Rockefeller/Git/SHED/backend/tests/fastqs/3fastqtest.fastq.gz'))
+        # single and paired
+        assert file_parse.find_fastqs(base_path, "3fastqtest") == (
+            (
+                f"{base_path}fastqs/3fastqtest_1.fastq.gz",
+                f"{base_path}fastqs/3fastqtest_2.fastq.gz",
+                f"{base_path}fastqs/3fastqtest.fastq.gz",
+            )
+        )
