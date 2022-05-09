743701fdc75aecd257afbfda570e8a679b92150c
degregory <virologistd@gmail.com>
2022-05-09 09:10:58 -0500

    polish, finishing tests

--- backend/tests/xfull_length_test.py
+++ backend/tests/xfull_length_test.py
@@ -15,12 +15,32 @@ class TestFullPipeline:
 
     def test_full_run(self):
         assert os.system(f"python3 {TestPath[:-5]}pipeline.py -d {TestPath} -i {TestPath}/lists/FullTestList.txt") == 0
-        
-    # def test_lineage_agg
-    
-    
-    # def test_covar_agg
-    
-    
-    # def test_consensus_agg
-    
+
+    # test aggregate outputs
+
+    def test_lineage_agg(self):
+        with open(f"{TestPath}/FullLins.tsv", "r") as checked:
+            with open(f"{TestPath}/Lineages.tsv", "r") as good:
+                with open(f"{TestPath}/SoloFullLins.tsv", "r") as solocheck:
+                    good_read = good.read()
+                    file_check1 = checked.read() ==  good_read
+                    file_check2 = solocheck.read() == good_read
+                    assert file_check1 or file_check2
+
+    def test_covar_agg(self):
+        with open(f"{TestPath}/FullNTs.tsv", "r") as checked:
+            with open(f"{TestPath}/NT_Calls.tsv", "r") as good:
+                with open(f"{TestPath}/SoloFullNTs.tsv", "r") as solocheck:
+                    good_read = good.read()
+                    file_check1 = checked.read() ==  good_read
+                    file_check2 = solocheck.read() == good_read
+                    assert file_check1 or file_check2
+
+    def test_consensus_agg(self):
+        with open(f"{TestPath}/FullCons.tsv", "r") as checked:
+            with open(f"{TestPath}/Consensus.tsv", "r") as good:
+                with open(f"{TestPath}/SoloFullCons.tsv", "r") as solocheck:
+                    good_read = good.read()
+                    file_check1 = checked.read() ==  good_read
+                    file_check2 = solocheck.read() == good_read
+                    assert file_check1 or file_check2
