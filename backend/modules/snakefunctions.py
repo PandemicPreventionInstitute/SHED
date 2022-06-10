f9f662664858f64392f94909126b7211239f3b41
Zachary Susswein <zsusswein@gmail.com>
2022-06-10 14:02:53 +0200

    Clean up aggregate_endpoints()

--- backend/modules/snakefunctions.py
+++ backend/modules/snakefunctions.py
@@ -82,6 +82,9 @@ def get_primer_bed(primers_str: str) -> str:
     """
     Called to determine the proper primer bed based on the str passed.
 
+    NOTE: This function only matches the first key in the dict. Later keys
+    that match will be ignored.
+
     Parameters:
     primers_str - string of potential primer info - str
 
@@ -96,32 +99,16 @@ def get_primer_bed(primers_str: str) -> str:
     with open("data/primer_mapping.json", encoding="utf-8") as _primer_map_json:
         _primer_map_dict = json.load(_primer_map_json)
 
-    # keys = _primer_map_dict.keys()
-    # contains_key = keys in primers_str
-
-    # if sum(contains_key) == 1:  # if exactly one match
-
-    # bed = bed.get(keys[contains_key], "Unknown")
-
-    # # If the match is a subdict, iterate through it  match there necessary
-    # while isinstance(bed, dict):
-
-    # keys = bed.keys()
-    # contains_key = keys in primers_str
-    # bed = bed.get(keys[contains_key], "Unknown")
-
-    # else:
-
-    # bed = "Unknown"
-
     bed = "Unknown"
     matching_dict = _primer_map_dict
     while isinstance(matching_dict, dict) and bed == "Unknown":
         for key in matching_dict:
             if key in primers_str:
+                # Multiple keys may be present in primers_str. Match the first
+                # key only.
                 if isinstance(_primer_map_dict[key], str):
                     bed = _primer_map_dict[key]
-                    break  # multiple keys may be present in primers_str, match the first key only
+                    break
                 matching_dict = matching_dict[key]
             else:
                 try:
@@ -152,7 +139,7 @@ def start_ele_fun(name, attrs, element_strs, elements_dict, out_fh):
 
     if len(element_strs) > 1:
         if len(element_strs) > 2:
-            for elems in element_strs[2:]:
+            for _ in element_strs[2:]:
                 out_fh.write("    ")
         out_fh.write(name)
         out_fh.write(" :")
@@ -169,13 +156,12 @@ def start_ele_fun(name, attrs, element_strs, elements_dict, out_fh):
     return elements_dict
 
 
-def end_ele_fun(name, element_strs, elements_dict, out_fh, lite_out_fh):
+def end_ele_fun(element_strs, elements_dict, out_fh, lite_out_fh):
     """
     Called to perform the actions and logic of dealing with the end
     of an xml element.
 
     Parameters:
-    name - name of the element - str
     element_strs - list of the elements/subelements - list
     elements_dict - dict to collect metadata from elements - dict
     out_fh - file to write out all metadata - file object
@@ -226,17 +212,16 @@ def data_fun(data, element_strs, elements_dict, flags, out_fh):
     """
 
     if len(element_strs) > 1:
-        for elems in element_strs[1:]:
+        for _ in element_strs[1:]:
             out_fh.write("    ")
     out_fh.write(data)
     out_fh.write("\n")
-    if flags["loc"]:
-        if element_strs[-1] == "VALUE":
-            if elements_dict["loc"]:
-                elements_dict["loc"] += ", " + data
-            else:
-                elements_dict["loc"] = data
-        flags["loc"] = False
+    if flags["loc"] and element_strs[-1] == "VALUE":
+        if elements_dict["loc"]:
+            elements_dict["loc"] += ", " + data
+        else:
+            elements_dict["loc"] = data
+    flags["loc"] = False
     if element_strs[-1] == "TAG" and (
         data == "geo_loc_name"
         or data == "geo loc name"
@@ -326,9 +311,9 @@ def parse_xml_meta(date_stamp: str) -> int:
                     element_strs.append(name)
                     start_ele_fun(name, attrs, element_strs, elements_dict, out_fh)
 
-                def end_element(name):
+                def end_element():
                     element_strs.pop()
-                    end_ele_fun(name, element_strs, elements_dict, out_fh, lite_out_fh)
+                    end_ele_fun(element_strs, elements_dict, out_fh, lite_out_fh)
 
                 def char_data(data):
                     if data and data.strip():
@@ -348,6 +333,10 @@ def parse_xml_meta(date_stamp: str) -> int:
 def get_sample_acc1(redo: bool) -> list:
     """
     Called to get the accessions of the samples of the current query.
+
+    TODO: This function still defines three handler functions within this
+    function definition and so will redefine them each time this function is
+    called. Fix by moving the handler functions above.
 
     Parameters:
     bool - boolean for reprocessing samples - bool
@@ -450,6 +439,88 @@ def qc_pass(sample_accs: dict) -> list:
     return passed
 
 
+def add_sample(agg, file, samp):
+    """
+    This function add the sample to the file
+    """
+
+    with open(file, "r", encoding="utf-8") as samp_fh:
+        agg.write(samp)
+        agg.write("\n")
+        agg.write(samp_fh.read())
+        agg.write("\n--------\n")
+
+
+def check_if_present(agg, redo, file, samp):
+    """
+    This function checks if the sample is present in the aggregated file.
+    """
+
+    agg.seek(0)
+    present = samp in agg.read()
+    re_write = False
+
+    if redo and present:
+        re_write = True
+    elif not present:
+        add_sample(agg, file, samp)
+
+    return re_write
+
+
+def re_write_tsv_file(re_write_file, samp, file):
+    """
+    This function takes the variant call or lineage call tsv file and adds the
+    new variant calls as necessary.
+    """
+
+    old_file = ""
+    new_file = ""
+
+    with open(re_write_file, "r", encoding="utf-8") as agg:
+        old_file = agg.read()
+    old_split = old_file.split("\n--------\n")
+
+    for data in old_split:
+        if (not data.startswith(samp)) and data.strip():
+            new_file += data
+            new_file += "\n--------\n"
+
+    with open(re_write_file, "w", encoding="utf-8") as agg:
+        agg.write(new_file)
+        with open(file, "r", encoding="utf-8") as samp_fh:
+            agg.write(samp)
+            agg.write("\n")
+            agg.write(samp_fh.read())
+            agg.write("\n--------\n")
+
+
+def re_write_consensus_file(con_file, samp, file):
+    """
+    This function re-writes the consensus file if necessary. This function is
+    separate from the more general re_write_tsv_file() because the consensus
+    is in fastq format.
+    """
+
+    old_file = ""
+    new_file = ""
+
+    with open(con_file, "r", encoding="utf-8") as agg:
+        old_file = agg.read()
+
+    old_split = old_file.split(">")
+
+    for data in old_split:
+        if (not samp in data) and data.strip():
+            new_file += ">"
+            new_file += data
+
+    with open(con_file, "w", encoding="utf-8") as agg:
+        agg.write(new_file)
+        with open(file, "r", encoding="utf-8") as samp_fh:
+            agg.write(samp_fh.read())
+
+
 def aggregate_endpoints(vc_list, con_list, lin_list, redo) -> int:
     """
     Called to aggregate the individual sample endpoints.
@@ -467,94 +538,45 @@ def aggregate_endpoints(vc_list, con_list, lin_list, redo) -> int:
     Returns 0 if successful.
     """
 
+    vc_file = "endpoints/VCs.tsv"
+    con_file = "endpoints/Consensus.fa"
+    lineage_file = "endpoints/Lineages.tsv"
+
+    ####################
+    # Iterate through variant calls, adding to summary file if not yet present
+
     for file in vc_list:
         samp = file.split("/")[1].split(".")[0]
-        re_write = False
-        with open("endpoints/VCs.tsv", "a+") as agg:
-            agg.seek(0)
-            present = samp in agg.read()
-            if redo and present:
-                re_write = True
-            elif not present:
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp)
-                    agg.write("\n")
-                    agg.write(samp_fh.read())
-                    agg.write("\n--------\n")
+
+        with open(vc_file, "a+", encoding="utf-8") as agg:
+            re_write = check_if_present(agg, redo, file, samp)
+
         if re_write:
-            old_file = ""
-            new_file = ""
-            with open("endpoints/VCs.tsv", "r", encoding="utf-8") as agg:
-                old_file = agg.read()
-            old_split = old_file.split("\n--------\n")
-            for data in old_split:
-                if (not data.startswith(samp)) and data.strip():
-                    new_file += data
-                    new_file += "\n--------\n"
-            with open("endpoints/VCs.tsv", "w", encoding="utf-8") as agg:
-                agg.write(new_file)
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp)
-                    agg.write("\n")
-                    agg.write(samp_fh.read())
-                    agg.write("\n--------\n")
+            re_write_tsv_file(vc_file, samp, file)
+
+    ####################
+    # Iterate through consensus definitions, adding to summary file if not yet
+    # present
 
     for file in con_list:
         samp = file.split("/")[1].split(".")[0]
-        re_write = False
-        with open("endpoints/Consensus.fa", "a+") as agg:
-            agg.seek(0)
-            present = samp in agg.read()
-            if redo and present:
-                re_write = True
-            elif not present:
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp_fh.read())
+
+        with open(con_file, "a+", encoding="utf-8") as agg:
+            re_write = check_if_present(agg, redo, file, samp)
+
         if re_write:
-            old_file = ""
-            new_file = ""
-            with open("endpoints/Consensus.fa", "r", encoding="utf-8") as agg:
-                old_file = agg.read()
-            old_split = old_file.split(">")
-            for data in old_split:
-                if (not samp in data) and data.strip():
-                    new_file += ">"
-                    new_file += data
-            with open("endpoints/Consensus.fa", "w", encoding="utf-8") as agg:
-                agg.write(new_file)
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp_fh.read())
+            re_write_consensus_file(con_file, samp, file)
+
+    ####################
+    # Iterate through lineages, adding to summary file if not yet present
 
     for file in lin_list:
         samp = file.split("/")[1].split(".")[0]
-        re_write = False
-        with open("endpoints/Lineages.tsv", "a+") as agg:
-            agg.seek(0)
-            present = samp in agg.read()
-            if redo and present:
-                re_write = True
-            elif not present:
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp)
-                    agg.write("\n")
-                    agg.write(samp_fh.read())
-                    agg.write("\n--------\n")
+
+        with open(lineage_file, "a+", encoding="utf-8") as agg:
+            re_write = check_if_present(agg, redo, file, samp)
+
         if re_write:
-            old_file = ""
-            new_file = ""
-            with open("endpoints/Lineages.tsv", "r", encoding="utf-8") as agg:
-                old_file = agg.read()
-            old_split = old_file.split("\n--------\n")
-            for data in old_split:
-                if (not data.startswith(samp)) and data.strip():
-                    new_file += data
-                    new_file += "\n--------\n"
-            with open("endpoints/Lineages.tsv", "w", encoding="utf-8") as agg:
-                agg.write(new_file)
-                with open(file, "r", encoding="utf-8") as samp_fh:
-                    agg.write(samp)
-                    agg.write("\n")
-                    agg.write(samp_fh.read())
-                    agg.write("\n--------\n")
+            re_write_tsv_file(lineage_file, samp, file)
 
     return 0
