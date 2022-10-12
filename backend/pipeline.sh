f2abe708fb0e0ea7b1911df0b01d8ed1da803605
Zachary Susswein <zsusswein@gmail.com>
2022-10-12 12:01:45 -0400

    Hardcode using a single core for Docker image

--- backend/pipeline.sh
+++ backend/pipeline.sh
@@ -4,10 +4,10 @@
 # bash to execute the 3 snakefiles in turn
 # Last edited on 7-10-22
 
-echo "Enter number of cores to use in running the pipeline"
+#echo "Enter number of cores to use in running the pipeline"
 # get how many cores should be used to run the workflow
-read -r cores
-
+# read -r cores
+cores=1
 workingdir=$( dirname -- "$( readlink -f -- "$0"; )"; )
 
 # Queries NCBI's SRA for matches to the config.yaml query string
